a2b075c4e4c30ac15ad42077dcdc032ec8510b16
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-10 18:29:27 +0530

    Constants, Manipulators in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,52 +1,34 @@
-#include<iostream> 
+#include<iostream>
+#include<iomanip>
 
 using namespace std;
 
-int c = 45;
-
 int main(){
-    
-    // *************Build in Data types****************
-    // int a, b, c;
-    // cout<<"Enter the value of a:"<<endl;
-    // cin>>a;
-    // cout<<"Enter the value of b:"<<endl;
-    // cin>>b;
-    // c = a + b;
-    // cout<<"The sum is "<<c<<endl;
-    // cout<<"The global c is "<<::c;
+    // int a = 34; 
+    // cout<<"The value of a was: "<<a;
+    // a = 45; 
+    // cout<<"The value of a is: "<<a;
+    // Constants in C++
+    // const int a = 3;
+    // cout<<"The value of a was: "<<a<<endl;
+    // a = 45; // You will get an error because a is a constant
+    // cout<<"The value of a is: "<<a<<endl;
 
-    // ************* Float, double and long double Literals****************
-    // float d=34.4F;
-    // long double e = 34.4L; 
-    // cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
-    // cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
-    // cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
-    // cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
-    // cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
-    // cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;  
+    // Manipulators in C++
+    // int a =3, b=78, c=1233;
+    // cout<<"The value of a without setw is: "<<a<<endl;
+    // cout<<"The value of b without setw is: "<<b<<endl;
+    // cout<<"The value of c without setw is: "<<c<<endl;
 
-    // *************Reference Variables**************** 
-    // Rohan Das----> Monty -----> Rohu ------> Dangerous Coder
-    // float x = 455;
-    // float & y = x;
-    // cout<<x<<endl;
-    // cout<<y<<endl;
-
-    // *************Typecasting**************** 
-    int a = 45;   
-    float b = 45.46;
-    cout<<"The value of a is "<<(float)a<<endl;
-    cout<<"The value of a is "<<float(a)<<endl;
-
-    cout<<"The value of b is "<<(int)b<<endl;
-    cout<<"The value of b is "<<int(b)<<endl;
-    int c = int(b);
-
-    cout<<"The expression is "<<a + b<<endl;
-    cout<<"The expression is "<<a + int(b)<<endl;
-    cout<<"The expression is "<<a + (int)b<<endl;
+    // cout<<"The value of a is: "<<setw(4)<<a<<endl;
+    // cout<<"The value of b is: "<<setw(4)<<b<<endl;
+    // cout<<"The value of c is: "<<setw(4)<<c<<endl;
 
 
+    // Operator Precedence
+    int a =3, b=4;
+    // int c = (a*5)+b;
+    int c = ((((a*5)+b)-45)+87);
+    cout<<c;
     return 0;
 }
