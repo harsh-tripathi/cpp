36d395ac50d990a16a1ba63662662d2ffdd311e1
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-10 18:30:49 +0530

    If Else and Switch-Case Statement in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,34 +1,45 @@
 #include<iostream>
-#include<iomanip>
 
 using namespace std;
 
 int main(){
-    // int a = 34; 
-    // cout<<"The value of a was: "<<a;
-    // a = 45; 
-    // cout<<"The value of a is: "<<a;
-    // Constants in C++
-    // const int a = 3;
-    // cout<<"The value of a was: "<<a<<endl;
-    // a = 45; // You will get an error because a is a constant
-    // cout<<"The value of a is: "<<a<<endl;
+    // cout<<"This is tutorial 9";
+    int age;
+    cout<< "Tell me your age"<<endl;
+    cin>>age;
 
-    // Manipulators in C++
-    // int a =3, b=78, c=1233;
-    // cout<<"The value of a without setw is: "<<a<<endl;
-    // cout<<"The value of b without setw is: "<<b<<endl;
-    // cout<<"The value of c without setw is: "<<c<<endl;
+    // 1. Selection control structure: If else-if else ladder
+    // if((age<18) && (age>0)){
+    //     cout<<"You can not come to my party"<<endl;
+    // }
+    // else if(age==18){
+    //     cout<<"You are a kid and you will get a kid pass to the party"<<endl;
+    // }
+    // else if(age<1){
+    //     cout<<"You are not yet born"<<endl;
+    // }
+    // else{
+    //     cout<<"You can come to the party"<<endl;
+    // }
 
-    // cout<<"The value of a is: "<<setw(4)<<a<<endl;
-    // cout<<"The value of b is: "<<setw(4)<<b<<endl;
-    // cout<<"The value of c is: "<<setw(4)<<c<<endl;
+    // 2. Selection control structure: Switch Case statements
+    switch (age)
+    {
+    case 18: 
+        cout<<"You are 18"<<endl;
+        break;
+    case 22: 
+        cout<<"You are 22"<<endl;
+        break;
+    case 2: 
+        cout<<"You are 2"<<endl;
+        break;
 
+    default:
+    cout<<"No special cases"<<endl;
+        break;
+    }
 
-    // Operator Precedence
-    int a =3, b=4;
-    // int c = (a*5)+b;
-    int c = ((((a*5)+b)-45)+87);
-    cout<<c;
+    cout<<"Done with switch case";
     return 0;
 }
