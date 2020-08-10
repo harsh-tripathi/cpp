d369c527002cb46edef76ba9117e8ed8d707b096
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-10 18:26:49 +0530

    Reference Variables & Typecasting in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,45 +1,51 @@
-// There are two types of header files:
-// 1. System header files: It comes with the compiler
-#include<iostream>
-// 2. User defined header files: It is written by the programmer
-// #include "this.h" //--> This will produce an error if this.h is no present in the current directory
+#include<iostream> 
 
 using namespace std;
 
+int c = 45;
+
 int main(){
-    int a=4, b=5;
-    cout<<"Operators in C++:"<<endl;
-    cout<<"Following are the types of operators in C++"<<endl;
-    // Arithmetic operators
-    cout<<"The value of a + b is "<<a+b<<endl;
-    cout<<"The value of a - b is "<<a-b<<endl;
-    cout<<"The value of a * b is "<<a*b<<endl;
-    cout<<"The value of a / b is "<<a/b<<endl;
-    cout<<"The value of a % b is "<<a%b<<endl;
-    cout<<"The value of a++ is "<<a++<<endl;
-    cout<<"The value of a-- is "<<a--<<endl;
-    cout<<"The value of ++a is "<<++a<<endl;
-    cout<<"The value of --a is "<<--a<<endl;
-    cout<<endl;
+    
+    // *************Build in Data types****************
+    // int a, b, c;
+    // cout<<"Enter the value of a:"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b:"<<endl;
+    // cin>>b;
+    // c = a + b;
+    // cout<<"The sum is "<<c<<endl;
+    // cout<<"The global c is "<<::c;
 
-    // Assignment Operators --> used to assign values to variables
-    // int a =3, b=9;
-    // char d='d';
+    // ************* Float, double and long double Literals****************
+    // float d=34.4F;
+    // long double e = 34.4L; 
+    // cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+    // cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    // cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    // cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    // cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
+    // cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;  
 
-    // Comparison operators
-    cout<<"Following are the comparison operators in C++"<<endl;
-    cout<<"The value of a == b is "<<(a==b)<<endl;
-    cout<<"The value of a != b is "<<(a!=b)<<endl;
-    cout<<"The value of a >= b is "<<(a>=b)<<endl;
-    cout<<"The value of a <= b is "<<(a<=b)<<endl;
-    cout<<"The value of a > b is "<<(a>b)<<endl;
-    cout<<"The value of a < b is "<<(a<b)<<endl; 
+    // *************Reference Variables**************** 
+    // Rohan Das----> Monty -----> Rohu ------> Dangerous Coder
+    // float x = 455;
+    // float & y = x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
 
-    // Logical operators
-    cout<<"Following are the logical operators in C++"<<endl;
-    cout<<"The value of this logical and operator ((a==b) && (a<b)) is:"<<((a==b) && (a<b))<<endl; 
-    cout<<"The value of this logical or operator ((a==b) || (a<b)) is:"<<((a==b) || (a<b))<<endl; 
-    cout<<"The value of this logical not operator (!(a==b)) is:"<<(!(a==b))<<endl; 
+    // *************Typecasting**************** 
+    int a = 45;   
+    float b = 45.46;
+    cout<<"The value of a is "<<(float)a<<endl;
+    cout<<"The value of a is "<<float(a)<<endl;
+
+    cout<<"The value of b is "<<(int)b<<endl;
+    cout<<"The value of b is "<<int(b)<<endl;
+    int c = int(b);
+
+    cout<<"The expression is "<<a + b<<endl;
+    cout<<"The expression is "<<a + int(b)<<endl;
+    cout<<"The expression is "<<a + (int)b<<endl;
 
 
     return 0;
