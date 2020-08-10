25ca5095b615a3b10fe9239ca0e933d3b0a9c735
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-10 18:36:32 +0530

    Pointers in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 
 int main(){
-    // for (int i = 0; i < 40; i++)
-    // {
-    //     /* code */
-    //     if(i==2){
-    //         break;
-    //     }
-    //     cout<<i<<endl;
-    // }
-    for (int i = 0; i < 40; i++)
-    {
-        /* code */
-        if(i==2){
-            continue;
-        }
-        cout<<i<<endl;
-    }
+    // What is a pointer? ----> Data type which holds the address of other data types
+    int a=3;
+    int* b;
+    b = &a;
 
-    
+    // & ---> (Address of) Operator
+    cout<<"The address of a is "<<&a<<endl;
+    cout<<"The address of a is "<<b<<endl;
+
+    // * ---> (value at) Dereference operator
+    cout<<"The value at address b is "<<*b<<endl;
+
+    // Pointer to pointer
+    int** c = &b;
+    cout<<"The address of b is "<<&b<<endl;
+    cout<<"The address of b is "<<c<<endl; 
+    cout<<"The value at address c is "<<*c<<endl; 
+    cout<<"The value at address value_at(value_at(c)) is "<<**c<<endl; 
+
     return 0;
 }
