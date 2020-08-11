0d1d9f99f4acb0783a2943cbfe0a223725171536
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-11 20:54:28 +0530

    Inline Functions, Default Arguments & Constant Arguments in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,42 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int sum(int a, int b){
-    int c = a + b;
-    return c;
+inline int product(int a, int b){
+    // Not recommended to use below lines with inline functions
+    // static int c=0; // This executes only once
+    // c = c + 1; // Next time this function is run, the value of c will be retained
+    return a*b;
 }
 
-// This will not swap a and b
-void swap(int a, int b){ //temp a b
-    int temp = a;        //4   4  5   
-    a = b;               //4   5  5
-    b = temp;            //4   5  4 
+float moneyReceived(int currentMoney, float factor=1.04){
+    return currentMoney * factor;
 }
 
-// Call by reference using pointers
-void swapPointer(int* a, int* b){ //temp a b
-    int temp = *a;          //4   4  5   
-    *a = *b;               //4   5  5
-    *b = temp;            //4   5  4 
-}
+// int strlen(const char *p){
 
-// Call by reference using C++ reference Variables
-// int & 
-void swapReferenceVar(int &a, int &b){ //temp a b
-    int temp = a;          //4   4  5   
-    a = b;               //4   5  5
-    b = temp;            //4   5  4 
-    // return a;
-}
-
+// }
 int main(){
-    int x =4, y=5;
-    // cout<<"The sum of 4 and 5 is "<<sum(a, b);
-    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
-    // swap(x, y); // This will not swap a and b
-    // swapPointer(&x, &y); //This will swap a and b using pointer reference
-    swapReferenceVar(x, y); //This will swap a and b using reference variables
-    // swapReferenceVar(x, y) = 766; //This will swap a and b using reference variables
-    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl; 
+    int a, b;
+    // cout<<"Enter the value of a and b"<<endl;
+    // cin>>a>>b;
+    // cout<<"The product of a and b is "<<product(a,b)<<endl;
+    int money = 100000;
+    cout<<"If you have "<<money<<" Rs in your bank account, you will recive "<<moneyReceived(money)<< "Rs after 1 year"<<endl;
+    cout<<"For VIP: If you have "<<money<<" Rs in your bank account, you will recive "<<moneyReceived(money, 1.1)<< " Rs after 1 year";
     return 0;
 }
