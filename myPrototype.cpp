d92a084ee5c85018a13e0a6dedbfc6bfa229759b
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-11 20:49:28 +0530

    Structures, Unions & Enums in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,45 +1,43 @@
 #include<iostream>
 using namespace std;
 
+typedef struct employee
+{
+    /* data */
+    int eId; //4
+    char favChar; //1
+    float salary; //4
+} ep;
+
+union money
+{
+    /* data */
+    int rice; //4
+    char car; //1
+    float pounds; //4
+};
+
+
 int main(){
-    // Array Example
-    int marks[] = {23, 45, 56, 89};
+    enum Meal{ breakfast, lunch, dinner};
+    Meal m1 = lunch;
+    cout<<(m1==2);
+    // cout<<breakfast;
+    // cout<<lunch;
+    // cout<<dinner; 
+    // union money m1;
+    // m1.rice = 34;
+    // m1.car = 'c';
+    // cout<<m1.car;
 
-    int mathMarks[4];
-    mathMarks[0] = 2278;
-    mathMarks[1] = 738;
-    mathMarks[2] = 378;
-    mathMarks[3] = 578;
-
-    cout<<"These are math marks"<<endl;
-    cout<<mathMarks[0]<<endl;
-    cout<<mathMarks[1]<<endl;
-    cout<<mathMarks[2]<<endl;
-    cout<<mathMarks[3]<<endl;
-
-    // You can change the value of an array
-    marks[2] = 455;
-    cout<<"These are marks"<<endl;
-    // cout<<marks[0]<<endl;
-    // cout<<marks[1]<<endl;
-    // cout<<marks[2]<<endl;
-    // cout<<marks[3]<<endl;
-
-    for (int i = 0; i < 4; i++)
-    {
-        cout<<"The value of marks "<<i<<" is "<<marks[i]<<endl;
-    }
-
-    // Quick quiz: do the same using while and do-while loops?
-
-    // Pointers and arrays
-    int* p = marks;
-    cout<<*(p++)<<endl;
-    cout<<*(++p)<<endl;
-    // cout<<"The value of *p is "<<*p<<endl;
-    // cout<<"The value of *(p+1) is "<<*(p+1)<<endl;
-    // cout<<"The value of *(p+2) is "<<*(p+2)<<endl;
-    // cout<<"The value of *(p+3) is "<<*(p+3)<<endl; 
-    
+    // ep harry;
+    // struct employee shubham;
+    // struct employee rohanDas;
+    // harry.eId = 1;
+    // harry.favChar = 'c';
+    // harry.salary = 120000000;
+    // cout<<"The value is "<<harry.eId<<endl; 
+    // cout<<"The value is "<<harry.favChar<<endl; 
+    // cout<<"The value is "<<harry.salary<<endl; 
     return 0;
 }
