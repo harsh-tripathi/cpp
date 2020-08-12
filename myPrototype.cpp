f8e7e6368a34f1f6cedaacc9c34b6a5c4e3e163f
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-12 20:24:28 +0530

    Classes, Public and Private access modifiers in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,36 +1,34 @@
 #include<iostream>
 using namespace std;
 
-int sum(float a, int b){
-    cout<<"Using function with 2 arguments"<<endl;
-    return a+b;
-}
+class Employee
+{
+    private:
+        int a, b, c;
+    public:
+        int d, e;
+        void setData(int a1, int b1, int c1); // Declaration
+        void getData(){
+            cout<<"The value of a is "<<a<<endl;
+            cout<<"The value of b is "<<b<<endl;
+            cout<<"The value of c is "<<c<<endl;
+            cout<<"The value of d is "<<d<<endl;
+            cout<<"The value of e is "<<e<<endl;
+        }
+};
 
-int sum(int a, int b, int c){
-    cout<<"Using function with 3 arguments"<<endl;
-    return a+b+c;
-}
-
-// Calculate the volume of a cylinder
-int volume(double r, int h){
-    return(3.14 * r *r *h);
-}
-
-// Calculate the volume of a cube
-int volume(int a){
-    return (a * a * a);
-}
-
-// Rectangular box
-int volume (int l, int b, int h){
-    return (l*b*h);
+void Employee :: setData(int a1, int b1, int c1){
+    a = a1;
+    b = b1;
+    c = c1;
 }
 
 int main(){
-    cout<<"The sum of 3 and 6 is "<<sum(3,6)<<endl;
-    cout<<"The sum of 3, 7 and 6 is "<<sum(3, 7, 6)<<endl;
-    cout<<"The volume of cuboid of 3, 7 and 6 is "<<volume(3, 7, 6)<<endl;
-    cout<<"The volume of cylinder of radius 3 and height 6 is "<<volume(3, 6)<<endl;
-    cout<<"The volume of cube of side 3 is "<<volume(3)<<endl;
+    Employee harry;
+    // harry.a = 134; -->This will throw error as a is private
+    harry.d = 34;
+    harry.e = 89;
+    harry.setData(1,2,4);
+    harry.getData();
     return 0;
 }
