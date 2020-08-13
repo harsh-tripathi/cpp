74ad7be3163eec3a836b9b588fed24a55b9166c3
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-13 23:45:27 +0530

    Array of Objects & Passing Objects as Function Arguments in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,49 +1,35 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Employee
-{
-    int id;
-    static int count;
+class complex{
+    int a;
+    int b;
 
-public:
-    void setData(void)
-    {
-        cout << "Enter the id" << endl;
-        cin >> id;
-        count++;
-    }
-    void getData(void)
-    {
-        cout << "The id of this employee is " << id << " and this is employee number " << count << endl;
-    }
+    public: 
+        void setData(int v1, int v2){
+            a = v1;
+            b = v2;
+        }
 
-    static void getCount(void){
-        // cout<<id; // throws an error
-        cout<<"The value of count is "<<count<<endl;
-    }
+        void setDataBySum(complex o1, complex o2){
+            a = o1.a + o2.a;
+            b = o1.b + o2.b;
+        }
+
+        void printNumber(){
+            cout<<"Your complex number is "<<a<<" + "<<b<<"i"<<endl;
+        }
 };
 
-// Count is the static data member of class Employee
-int Employee::count; // Default value is 0
+int main(){
+    complex c1, c2, c3;
+    c1.setData(1, 2);
+    c1.printNumber();
 
-int main()
-{
-    Employee harry, rohan, lovish;
-    // harry.id = 1;
-    // harry.count=1; // cannot do this as id and count are private
+    c2.setData(3, 4);
+    c2.printNumber();
 
-    harry.setData();
-    harry.getData();
-    Employee::getCount();
-
-    rohan.setData();
-    rohan.getData();
-    Employee::getCount();
-
-    lovish.setData();
-    lovish.getData();
-    Employee::getCount();
-
+    c3.setDataBySum(c1, c2);
+    c3.printNumber();
     return 0;
 }
