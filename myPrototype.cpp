28e46675d687cb6bc2e67d5519e55b1a4ed8bed1
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-14 23:17:55 +0530

    Parameterized and Default Constructors In C++ 1.0

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
 
 class Complex
 {
     int a, b;
 
 public:
-    // Creating a Constructor
-    // Constructor is a special member function with the same name as of the class.
-    //It is used to initialize the objects of its class
-    //It is automatically invoked whenever an object is created
-
-    Complex(void); // Constructor declaration
+    Complex(int, int); // Constructor declaration
 
     void printNumber()
     {
@@ -19,30 +15,21 @@ public:
     }
 };
 
-Complex ::Complex(void) // ----> This is a default constructor as it takes no parameters
+Complex ::Complex(int x, int y) // ----> This is a parameterized constructor as it takes 2 parameters
 {
-    a = 0;
-    b = 0;
+    a = x;
+    b = y;
     // cout<<"Hello world";
 }
 
-int main()
-{
-    Complex c1, c2, c3;
-    c1.printNumber();
-    c2.printNumber();
-    c3.printNumber();
+int main(){
+    // Implicit call
+    Complex a(4, 6);
+    a.printNumber();
+
+    // Explicit call
+    Complex b = Complex(5, 7);
+    b.printNumber();
 
     return 0;
 }
-
-
-/*  Characteristics of Constructors
-
-1. It should be declared in the public section of the class 
-2. They are automatically invoked whenever the object is created 
-3. They cannot return values and do not have return types
-4. It can have default arguments 
-5. We cannot refer to their address
-
-*/
