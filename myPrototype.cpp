14621bbcdc52ef91b4d6f7d9b0452d4472f1deb8
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-14 23:18:44 +0530

    Parameterized and Default Constructors In C++ 2.0

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,35 +1,30 @@
 #include<iostream>
 using namespace std;
 
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
 
-class Complex
-{
-    int a, b;
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
 
-public:
-    Complex(int, int); // Constructor declaration
-
-    void printNumber()
-    {
-        cout << "Your number is " << a << " + " << b << "i" << endl;
-    }
 };
+// Create a function (Hint: Make it a friend function) which takes 2 point objects and computes the distance between those 2 points
 
-Complex ::Complex(int x, int y) // ----> This is a parameterized constructor as it takes 2 parameters
-{
-    a = x;
-    b = y;
-    // cout<<"Hello world";
-}
-
+// Use these examples to check your code:
+// Distance between (1, 1) and (1, 1) is 0
+// Distance between (0, 1) and (0, 6) is 5
+// Distance between (1, 0) and (70, 0) is 69
 int main(){
-    // Implicit call
-    Complex a(4, 6);
-    a.printNumber();
+    Point p(1, 1);
+    p.displayPoint();
 
-    // Explicit call
-    Complex b = Complex(5, 7);
-    b.printNumber();
-
+    Point q(4, 6);
+    q.displayPoint();
     return 0;
 }
