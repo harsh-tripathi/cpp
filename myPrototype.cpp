46f6bd892e8e0df35d5efb1209eac9b64eeb28d4
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-15 22:52:30 +0530

    Constructors With Default Arguments In C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,43 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Complex
-{
-    int a, b;
+class Simple{
+    int data1;
+    int data2;
+    int data3;
 
-public:
-    Complex(){
-        a = 0;
-        b =0;
-    }
+    public:
+        Simple(int a, int b=9, int c=8){
+            data1 = a;
+            data2 = b;
+            data3 = c;
+        }
 
-    Complex(int x, int y)
-    {
-        a = x;
-        b = y;
-    }
+        void printData();
 
-    Complex(int x){
-        a = x;
-        b = 0;
-    }
-
-  
-
-    void printNumber()
-    {
-        cout << "Your number is " << a << " + " << b << "i" << endl;
-    }
 };
-int main()
-{
-    Complex c1(4, 6);
-    c1.printNumber();
 
-    Complex c2(5);
-    c2.printNumber();
+void Simple :: printData(){
+    cout<<"The value of data1, data2 and data3 is "<<data1<<", "<< data2<<" and "<< data3<<endl;
+}
 
-    Complex c3;
-    c3.printNumber();
+int main(){
+    Simple s(12, 13);
+    s.printData();
     return 0;
 }
