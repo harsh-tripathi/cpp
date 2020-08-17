bb5f2351386f9cd15e1e31f48bbdb0d4d1a48f98
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-18 00:43:46 +0530

    Protected Access Modifier in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,59 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Base
-{
-    int data1; // private by default and is not inheritable
-public:
-    int data2;
-    void setData();
-    int getData1();
-    int getData2();
+class Base{
+    protected:
+        int a; 
+    private:
+        int b;
+
+};
+/*
+For a protected member:
+                        Public derivation   Private Derivation   Protected Derivation
+    1. Private members      Not Inherited   Not Inherited       Not Inherited
+    2. Protected members    Protected       Private             Protected
+    3. Public members       Public          Private             Protected
+*/
+class Derived: protected Base{
+   
 };
 
-void Base ::setData(void)
-{
-    data1 = 10;
-    data2 = 20;
-}
-
-int Base::getData1()
-{
-    return data1;
-}
-
-int Base::getData2()
-{
-    return data2;
-}
-
-class Derived : public Base
-{ // Class is being derived publically
-    int data3;
-
-public:
-    void process();
-    void display();
-};
-
-void Derived ::process()
-{
-    data3 = data2 * getData1();
-}
-
-void Derived ::display()
-{
-    cout << "Value of data 1 is " << getData1() << endl;
-    cout << "Value of data 2 is " << data2 << endl;
-    cout << "Value of data 3 is " << data3 << endl;
-}
-
-int main()
-{
-    Derived der;
-    der.setData();
-    der.process();
-    der.display();
-
+int main(){
+    Base b;
+    Derived d;
+    // cout<<d.a; // Will not work since a is protected in both base as well as derived class
     return 0;
 }
