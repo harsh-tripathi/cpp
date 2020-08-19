1ca0efae046157762af03cbe5cba3fd28ef2f9fb
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-19 20:11:43 +0530

    Ambiguity Resolution in Inheritance in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,93 +1,62 @@
-#include <iostream>
-#include <math.h>
-#include <cmath>
-#include <stdlib.h>
-
-//Calculator using C++
-
+#include<iostream>
 using namespace std;
 
-const double pi = 3.14259;
-
-class simpleCalc
-{
-protected:
-    float x, y, add, multiple, substract, divide;
-
-public:
-    void getNumber(float, float);
-    void performAction(void);
-    void displayResult(void);
+class Base1{
+    public:
+        void greet(){
+            cout<<"How are you?"<<endl;
+        }
 };
 
-void simpleCalc ::getNumber(float a, float b)
-{
-    x = a;
-    y = b;
-}
-
-void simpleCalc ::performAction()
-{
-    add = x + y;
-    substract = x - y;
-    multiple = x * y;
-    divide = x / y;
-}
-
-void simpleCalc ::displayResult()
-{
-    cout << "The Addition of given numbers is : " << add << endl;
-    cout << "The Substraction of given numbers is : " << substract << endl;
-    cout << "The Multiplication of given numbers is : " << multiple << endl;
-    cout << "The Division of given numbers is : " << divide << endl;
-}
-
-class scientificCalc
-{
-protected:
-    double a, b, sine, cosine, tan, log;
-
-public:
-    void getInput(float, float);
-    void performScientificAction(void);
-    void displayOutput(void);
+class Base2{
+    public:
+        void greet()
+        {
+            cout << "Kaise ho?" << endl;
+        }
 };
 
-void scientificCalc ::getInput(float q, float r)
-{
-    a = q;
-    b = r;
-}
 
-void scientificCalc ::performScientificAction()
-{
-    sine = sin(a);
-    cosine = cos(a);
-    tan = tanh(a);
-    log = log10(b);
-}
-
-void scientificCalc ::displayOutput()
-{
-    cout << "The Sine of given number is : " << sine << endl;
-    cout << "The cosine of given number is : " << cosine << endl;
-    cout << "The tan of given number is : " << tan << endl;
-    cout << "The log with base 10 of given number is : " << log << endl;
-}
-
-class hybridCalc : public simpleCalc, public scientificCalc
-{
+class Derived : public Base1, public Base2{
+   int a;
+   public:
+    void greet(){
+        Base2 :: greet();
+    }
 };
 
-int main()
-{
-    hybridCalc h1;
-    h1.getNumber(5.0, 2.0);
-    h1.performAction();
-    h1.displayResult();
+class B{
+    public:
+        void say(){
+            cout<<"Hello world"<<endl;
+        }
+};
 
-    h1.getInput(pi / 4, 100);
-    h1.performScientificAction();
-    h1.displayOutput();
+class D: public B{
+    int a;
+    // D's new say() method will override base class's say() method
+    public:
+        void say()
+        {
+            cout << "Hello my beautiful people" << endl;
+        }
+};
+
+int main(){
+    // Ambibuity 1
+    // Base1 base1obj;
+    // Base2 base2obj;
+    // base1obj.greet();
+    // base2obj.greet();
+    // Derived d;
+    // d.greet();
+
+    // Ambibuity 2
+    B b;
+    b.say();
+
+    D d;
+    d.say();
+
     return 0;
 }
