ee53c6061c26012c6e4d0bc4cb1ddd508bbe9609
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-22 20:17:18 +0530

    Initialization list in Constructors in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,67 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 /*
-Case1:
-class B: public A{
-   // Order of execution of constructor -> first A() then B()
-};
+Syntax for initialization list in constructor:
+constructor (argument-list) : initilization-section
+{
+    assignment + other code;
+}
 
-Case2:
-class A: public B, public C{
-    // Order of execution of constructor -> B() then C() and A()
-};
-
-Case3:
-class A: public B, virtual public C{
-    // Order of execution of constructor -> C() then B() and A()
+class Test{
+    int a;
+    int b;
+    public:
+        Test(int i, int j) : a(i), b(j){constructor-body}
 };
 
 */
+class Test
+{
+    int a;
+    int b;
 
-class Base1{
-    int data1;
-    public:
-        Base1(int i){
-            data1 = i;
-            cout<<"Base1 class constructor called"<<endl;
-        }
-        void printDataBase1(void){
-            cout<<"The value of data1 is "<<data1<<endl;
-        }
+public:
+    // Test(int i, int j) : a(i), b(j)
+    // Test(int i, int j) : a(i), b(i+j)
+    // Test(int i, int j) : a(i), b(2 * j)
+    // Test(int i, int j) : a(i), b(a + j)
+    // Test(int i, int j) : b(j), a(i+b) -->RED Flag this will create problems because a will be initialized first
+    Test(int i, int j)
+    {
+        a = i;
+        b = j;
+        cout << "Constructor executed"<<endl;
+        cout << "Value of a is "<<a<<endl;
+        cout << "Value of b is "<<b<<endl;
+    }
 };
 
-class Base2{
-    int data2;
+int main()
+{
+    Test t(4, 6);
 
-    public:
-        Base2(int i){
-            data2 = i;
-            cout << "Base2 class constructor called" << endl;
-        }
-        void printDataBase2(void){
-            cout << "The value of data2 is " << data2 << endl;
-        }
-};
-
-class Derived: public Base2, public Base1{
-    int derived1, derived2;
-    public:
-        Derived(int a, int b, int c, int d) : Base2(b), Base1(a)
-        {
-            derived1 = c;
-            derived2 = d;
-            cout<< "Derived class constructor called"<<endl;
-        }
-        void printDataDerived(void)
-        {
-            cout << "The value of derived1 is " << derived1 << endl;
-            cout << "The value of derived2 is " << derived2 << endl;
-        }
-};
-int main(){
-    Derived harry(1, 2, 3, 4);
-    harry.printDataBase1();
-    harry.printDataBase2();
-    harry.printDataDerived();
     return 0;
 }
