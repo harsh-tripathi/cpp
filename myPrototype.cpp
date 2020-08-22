4b78c83a7d99948485848bbfc7c19f6fc3f1e8c4
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-22 20:18:52 +0530

    Pointers to Objects and Arrow Operator in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,28 +1,34 @@
 #include<iostream>
 using namespace std;
 
+class Complex{
+    int real, imaginary;
+    public:
+        void getData(){
+            cout<<"The real part is "<< real<<endl;
+            cout<<"The imaginary part is "<< imaginary<<endl;
+        }
+
+        void setData(int a, int b){
+            real = a;
+            imaginary = b;
+        }
+
+};
 int main(){
-    // Basic Example
-    int a = 4;
-    int* ptr = &a;
-    *ptr = 999;
-    cout<<"The value of a is "<<*(ptr)<<endl;
+    // Complex c1;
+    // Complex *ptr = &c1;
+    Complex *ptr = new Complex;
+    // (*ptr).setData(1, 54); is exactly same as
+    ptr->setData(1, 54);
 
-    // new operator
-    // int *p = new int(40);
-    float *p = new float(40.78);
-    cout << "The value at address p is " << *(p) << endl;
+    // (*ptr).getData(); is as good as 
+    ptr->getData(); 
 
-    int *arr = new int[3];
-    arr[0] = 10;
-    *(arr+1) = 20;
-    arr[2] = 30;
-    // delete[] arr;
-    cout << "The value of arr[0] is " << arr[0] << endl;
-    cout << "The value of arr[1] is " << arr[1] << endl;
-    cout << "The value of arr[2] is " << arr[2] << endl;
 
-    // delete operator
-    
+    // Array of Objects
+    Complex *ptr1 = new Complex[4]; 
+    ptr1->setData(1, 4); 
+    ptr1->getData();
     return 0;
 }
