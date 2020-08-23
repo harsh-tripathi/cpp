e31d77ba3d297b213a2d9c93b41d20e965a429cd
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-23 23:13:48 +0530

    this Pointer in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,54 +1,23 @@
 #include<iostream>
 using namespace std;
-
-class ShopItem
-{
-    int id;
-    float price;
+class A{
+    int a;
     public:
-        void setData(int a, float b){
-            id = a;
-            price = b;
+        // A & setData(int a){
+        void setData(int a){
+            this->a = a;
+            // return *this;
         }
-        void getData(void){
-            cout<<"Code of this item is "<< id<<endl;
-            cout<<"Price of this item is "<<price<<endl;
+
+        void getData(){
+            cout<<"The value of a is "<<a<<endl;
         }
 };
-        // 1 2 3
-        //     ^
-        //     |
-        //     |
-        //     ptr
-        // ptrTemp
+
 int main(){
-    int size = 3;
-    // int *ptr = &size;
-    // int *ptr = new int [34];
-
-    // 1. general store item
-    // 2. veggies item
-    // 3. hardware item
-    ShopItem *ptr = new ShopItem [size];
-    ShopItem *ptrTemp = ptr;
-    int p, i;
-    float q;
-    for (i = 0; i < size; i++)
-    {
-        cout<<"Enter Id and price of item "<< i+1<<endl;
-        cin>>p>>q;
-        // (*ptr).setData(p, q);
-        ptr->setData(p, q);
-        ptr++; 
-    }
-
-    for (i = 0; i < size; i++)
-    {
-        cout<<"Item number: "<<i+1<<endl;
-        ptrTemp->getData();
-        ptrTemp++;
-    }
-    
-    
+    // this is a keyword which is a pointer which points to the object which invokes the member function
+    A a;
+    a.setData(4);
+    a.getData();
     return 0;
 }
