dc42cd84ab557c3be415cfef83d4229dff87c662
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-25 23:46:09 +0530

    Virtual Functions and Run TIME POLYMORPHISM in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
 using namespace std;
-//Base class's pointer can be used by derieved class pointer
-//But if any inherited function is called through pointer it will call base class pointer only!
-//run-time polymorphism
-
+//Virtual Functions 
 class BaseClass
 {
 public:
-    int varbase;
-    void display()
+    int varbase = 1;
+    virtual void display() // virtual functions helps to achieve runtime polymorphism
     {
         cout << "This is a Base class variable " << varbase << endl;
     }
@@ -18,7 +15,7 @@ public:
 class DerievedClass : public BaseClass
 {
 public:
-    int varderieved;
+    int varderieved = 2;
     void display()
     {
         cout << "This is a Base class variable " << varderieved << endl;
@@ -28,16 +25,9 @@ public:
 int main()
 {
     BaseClass * base_pointer;
-    BaseClass obj_base;
-    DerievedClass obj_derieved;
-    base_pointer = &obj_derieved; //Base class pointer pointing to derieved class pointer
-    base_pointer->varbase = 55;
-    // base_pointer->varderieved = 5; // it will through error 
-    base_pointer->display(); // late binding - it will run base class function
-
-    DerievedClass * derieved = &obj_derieved;
-    derieved->varderieved = 111;
-    derieved->display();
-
+    BaseClass baseobj;
+    DerievedClass derobj;
+    base_pointer = &derobj;
+    base_pointer->display();//it will call derieved class method.
     return 0;
 }
