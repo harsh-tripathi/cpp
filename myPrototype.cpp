6055ba83a9b764d51e7784ce57e483ca03a852f9
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-28 23:07:48 +0530

    Some More Virtual Functions in C++

--- myPrototype.cpp
+++ myPrototype.cpp
@@ -1,33 +1,76 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
-//Virtual Functions 
-class BaseClass
+
+class Ayush
 {
+protected:
+    string title;
+    float num;
+
 public:
-    int varbase = 1;
-    virtual void display() // virtual functions helps to achieve runtime polymorphism
+    Ayush(String s, float r)
     {
-        cout << "This is a Base class variable " << varbase << endl;
+        strcpy(title, s)
+        {
+            num = r;
+        }
     }
+    virtual void display() {}
 };
 
-class DerievedClass : public BaseClass
+class AyushVideo : public Ayush
 {
+    int videolength;
+
 public:
-    int varderieved = 2;
+    AyushVideo(String s, float r, int vl) : CWH(s, r)
+    {
+        videolength = vl;
+    }
     void display()
     {
-        cout << "This is a Base class variable " << varderieved << endl;
+        cout << "This is title " << title << endl;
+        cout << "This is Rating " << num << endl;
+        cout << "This is video length " << vl << endl;
+    }
+};
+class AyushText : public Ayush
+{
+    int text;
+
+public:
+    AyushText(String s, float r, int wc) : CWH(s, r)
+    {
+        text = wc;
+    }
+    void display()
+    {
+
+        cout << "This is a title " << title << endl;
+        cout << "This is a Rating " << num << endl;
+        cout << "This is text length " << wc << endl;
     }
 };
 
 int main()
 {
-    BaseClass * base_pointer;
-    BaseClass baseobj;
-    DerievedClass derobj;
-    base_pointer = &derobj;
-    base_pointer->display();//it will call derieved class method.
-    return 0;
+    String title;
+    float rating, vlen;
+
+    title = "This is title";
+    vlen = 44;
+    rating = 3;
+
+    AyushVideo Video(title, rating, vlen);
+    Video.display();
 }
+//Rules for definind Virtual Function
+/*
+1. They are not static.
+2. They are accessed by pointers.
+3. Virtual function can be a friend of another class.
+4. A virtual funtion in a Base class might not be used.
+5. If a virtual function defined in the base class it is not necessary to redefine it. It will call the base class virtual function.
+*/
